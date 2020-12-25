f348aef24c78bcc1e3f741e456ce92f97c494fa9
lizza <liutao_glory@126.com>
2020-12-25 12:26:47 +0800

    lizza commit.

---
+++ High-Frequency/src/main/java/TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和
+ */
+public class TwoSum {
+
+    public static void main(String[] args){
+        int[] array = new int[]{3, 3};
+        int target = 6;
+        int[] result = twoSum(target, array);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int target, int[] array) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int e = array[i];
+            if (map.containsKey(target - e) && i != map.get(target - e)) {
+                return new int[]{map.get(target - e), i};
+            }
+            map.put(e, i);
+        }
+        return null;
+    }
+}
